d448fd3e2857acc4c6b12567c361f62d70fa8370
Kevin Matsunaga <kevinmillerjr@gmail.com>
2023-04-18 19:45:41 -0700

    Commit before deploying to Vercel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'wpblog';
+export class AppComponent implements OnInit {
+  posts = [];
+ 
+  constructor(private http: HttpClient){}
+ 
+  ngOnInit(): void{
+    this.http.get('https://headless.sasebo.tech/wp-json/wp/v2/posts').subscribe(data =>{
+      for(let key in data){
+        if(data.hasOwnProperty(key)){
+          this.posts.push(data[key]);
+        }
+      }
+      console.log(this.posts);
+    })
+  }
+ 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
